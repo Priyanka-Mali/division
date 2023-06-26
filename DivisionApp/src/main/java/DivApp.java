2dea1abc51401d462e0346c4d243ca8a01815d0c
Prashant <Prashant@DESKTOP-GK6FVNS>
2023-06-26 09:38:42 +0530

    first commit

---
+++ DivisionApp/src/main/java/DivApp.java
@@ -0,0 +1,11 @@
+
+public class DivApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=5;
+		System.out.print("division is = "+a/b);
+	}
+
+}
